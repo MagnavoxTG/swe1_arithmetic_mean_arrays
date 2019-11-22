9b4c84504200597a478ba3d3a1ec17ff0a0336c4
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-22 18:59:55 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/arithmetic_mean_arrays.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arithmetic_mean_arrays.dir/main.c.o"
+  "arithmetic_mean_arrays"
+  "arithmetic_mean_arrays.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arithmetic_mean_arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
